728bac99bdb1544861aa6b4003c7a4358e919fd1
Eddie Sandler <ed.sandler@gmail.com>
2023-07-18 18:45:34 -0400

    serial done

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,18 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self,start=100):
+        self.start=start
+        self.has_run=False
+
+    def generate(self):
+        if self.has_run:
+            self.start +=1
+        self.has_run = True
+        return self.start
+
+    def reset(self):
+        self.start = 100
+        self.has_run =False
+
+
