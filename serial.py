cf8c94b236e870cbaed7e36bf8016af2d105f832
Eddie Sandler <ed.sandler@gmail.com>
2023-07-18 19:18:18 -0400

    correction to serial.py

--- serial.py
+++ serial.py
@@ -17,18 +17,31 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
-    def __init__(self,start=100):
-        self.start=start
-        self.has_run=False
+
+    def __init__(self, start):
+        """Defines   serialGenerator   properties. sets initial
+        value to 100 and flag indicating whether the generate function
+        has run .(initially False)
+
+        """
+        self.start = start
+        self.num=start
+        self.has_run = False
 
     def generate(self):
+        """ Function that increments the serialGenerator value by 1 if it
+        hasn't run  previously.  Sets the has_run flag to True if it has run
+        previously.
+
+        """
         if self.has_run:
-            self.start +=1
+            self.num += 1
         self.has_run = True
-        return self.start
+        return self.num
 
     def reset(self):
-        self.start = 100
-        self.has_run =False
+        """Function to reset the serial generator back
+        to its default value (100)"""
 
-
+        self.num = self.start
+        self.has_run = False
